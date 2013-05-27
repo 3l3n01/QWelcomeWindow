ac0c231689ac39deb087408557df4c5dcdfce6a5
Colin <c.duquesnoy@sirmogames.com>
2013-05-27 10:51:29 +0200

    Fix library name in licenses

--- qwelcomewindow/qt/QtGui.py
+++ qwelcomewindow/qt/QtGui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# QWelcomeWidget - An easy to customize welcome window
+# QWelcomeWindow - An easy to customize welcome window
 
 # Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
 #
